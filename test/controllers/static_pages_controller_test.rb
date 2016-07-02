8df315240a9a8289089b5c84be960ead95c06187
jogis <Jogis@itanguo.cn>
2016-07-03 00:59:40 +0800

    init

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "about | #{@base_title}"
+  end
+
+end
